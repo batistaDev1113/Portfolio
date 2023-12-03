0dc560879616f0e016a05a52692e2b34e7678e12
Yunior Batista <yuniorbatista1113@gmail.com>
2023-12-03 14:12:44 -0500

    added supabase as a database (#28)
    * added supabase as a database
    
    * fixes sentry config errors
    
    * fixes lint issue

---
+++ db/fetchData.ts
@@ -0,0 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Throw error if SUPABASE_URL or SUPABASE_KEY is missing
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
+  throw new Error("Missing SUPABASE_URL or SUPABASE_KEY");
+}
+
+// Initialize supabase
+const supabaseUrl: string = process.env.SUPABASE_URL;
+const supabaseKey: string = process.env.SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function fetchData() {
+  try {
+    const { data, error } = await supabase.from("projects").select("*");
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
