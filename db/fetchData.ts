10349b701bcc8d1c7d15e990eb56d86249e7f6ca
Yunior Batista <yuniorbatista1113@gmail.com>
2024-01-05 12:54:10 -0500

    updated supabase fetching order (#38)

--- db/fetchData.ts
+++ db/fetchData.ts
@@ -10,9 +10,14 @@ const supabaseUrl: string = process.env.SUPABASE_URL;
 const supabaseKey: string = process.env.SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const revalidate = 1;
+
 export async function fetchData() {
   try {
-    const { data, error } = await supabase.from("projects").select("*");
+    const { data, error } = await supabase
+      .from("projects")
+      .select("*")
+      .order("id", { ascending: true });
     if (error) throw error;
     return data;
   } catch (error) {
